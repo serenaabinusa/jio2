6494e1e375c24b15292f37abf331c386b6f2074a
serenaabinusa <107521539+serenaabinusa@users.noreply.github.com>
2022-06-21 18:21:23 -0700

    Update data.js

--- db/data.js
+++ db/data.js
@@ -49,4 +49,4 @@ const Brainly = mongoose.Schema({
 
 // export model
 
-module.exports = mongoose.model('sitemap_2015-03_1.xml', Brainly);
+module.exports = mongoose.model('sitemap_2015-04_1.xml', Brainly);
